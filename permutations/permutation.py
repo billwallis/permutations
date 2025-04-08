58035f70b1cf9ab3f43fb663617c921934116086
Bill <bill@thewallisfamily.co.uk>
2025-04-08 10:35:36 +0100

    feat!: switch to `requirements.txt`

--- permutations/permutation.py
+++ permutations/permutation.py
@@ -8,6 +8,16 @@ class Permutation:
     A _permutation_ is a bijective map from a set to itself.
     """
 
-    def __init__(self, mapping: dict):
+    def __init__(self, mapping: dict[int, int]):
         self.mapping = mapping
-        self.set = list(mapping.keys())
+
+    @property
+    def as_list(self) -> list:
+        k = next(iter(self.mapping))
+        l, v = [k], k
+        while True:
+            if (v := self.mapping[v]) == k:
+                break
+            l.append(v)
+
+        return l
